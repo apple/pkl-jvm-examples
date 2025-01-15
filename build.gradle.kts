ccb6780878a17e674bf87a17dd970c5606bef796
Daniel Chao <daniel.h.chao@gmail.com>
2025-01-15 05:49:15 -0800

    Set up copyright year ratcheting (#15)
    When files get updated, update the copyright year range as well.
    
    Also, update year in NOTICE.txt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,14 @@ plugins {
   id("com.diffplug.spotless") version ("6.23.3")
 }
 
+val originalRemoteName = System.getenv("PKL_ORIGINAL_REMOTE_NAME") ?: "origin"
+
 spotless {
+  ratchetFrom = "$originalRemoteName/main"
+
   val starBlockLicenseHeader = """
     /**
-     * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+     * Copyright © ${'$'}YEAR Apple Inc. and the Pkl project authors. All rights reserved.
      *
      * Licensed under the Apache License, Version 2.0 (the "License");
      * you may not use this file except in compliance with the License.
@@ -24,7 +28,7 @@ spotless {
     """.trimIndent()
   val lineCommentLicenseHeader = """
     //===----------------------------------------------------------------------===//
-    // Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+    // Copyright © ${'$'}YEAR Apple Inc. and the Pkl project authors. All rights reserved.
     //
     // Licensed under the Apache License, Version 2.0 (the "License");
     // you may not use this file except in compliance with the License.
